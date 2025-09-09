12bfa2fd0744840a299d0cf96f8437e8fe0f248a
OrTech-prog <oramasodi7@gmail.com>
2025-09-09 11:52:03 +0200

    Ofentse Cat

---
+++ Cat.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercises09_09;
+
+/**
+ *
+ * @author orama
+ */
+public class Cat {
+    
+    String name;
+    int age;
+
+   
+    public Cat() {
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    public static void main(String[] args) {
+       
+        Cat myCat = new Cat();
+
+        
+        System.out.println("Cat's name: " + myCat.name);
+        System.out.println("Age: " + myCat.age);
+    }
+}
