8d638b365453df9ca80defbad7fb9bd373988c7f
OrTech-prog <oramasodi7@gmail.com>
2025-09-09 11:51:43 +0200

    Ofentse Book

---
+++ Book.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercises09_09;
+
+/**
+ *
+ * @author orama
+ */
+public class Book {
+	String bookTitle;
+	String bookAuthor;
+	float bookPrice; 
+	
+	public Book() {
+		this.bookTitle = "ABCs"; 
+		this.bookAuthor = "John P"; 
+	} 
+	
+	
+}
